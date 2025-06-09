f471131338cdd53b98cc9019d240cf6802cc1d83
trgao <tianrun.gao@gmail.com>
2025-06-09 16:45:13 +0800

    Add translation to text box

--- Hako/Components/List/TextBox.swift
+++ Hako/Components/List/TextBox.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import Translation
 
 struct TextBox: View {
     @State private var isExpanded = false
     @State private var canBeExpanded = false
+    @State private var showTranslation = false
     private let title: String
     private let text: String?
     
@@ -23,7 +25,6 @@ struct TextBox: View {
             Section {
                 VStack {
                     Text(text)
-                        .textSelection(.enabled)
                         .multilineTextAlignment(.leading)
                         .lineLimit(isExpanded ? nil : 4)
                         .background {
@@ -39,6 +40,20 @@ struct TextBox: View {
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(5)
                         .lineSpacing(2)
+                        .contextMenu {
+                            Button {
+                                UIPasteboard.general.string = text
+                            } label: {
+                                Label("Copy", systemImage: "document.on.document")
+                            }
+                            Button {
+                                showTranslation = true
+                            } label: {
+                                Label("Translate", systemImage: "translate")
+                            }
+                        }
+                        .translationPresentation(isPresented: $showTranslation,
+                                                         text: text)
                     if canBeExpanded {
                         Button {
                             isExpanded.toggle()

4a7859d7309e51ba35b93abc68eab1b105773ba3
trgao <tianrun.gao@gmail.com>
2025-06-15 18:46:25 +0800

    Fix airing at date format

--- Hako/Components/Anime/AnimeAiringInformation.swift
+++ Hako/Components/Anime/AnimeAiringInformation.swift
@@ -18,7 +18,7 @@ struct AnimeAiringInformation: View {
         if let nextEpisode = nextEpisode {
             Section {
                 ListRow(title: "Next episode", content: nextEpisode.episode)
-                ListRow(title: "Airing at", content: Date(timeIntervalSince1970: TimeInterval(nextEpisode.airingAt)).toString())
+                ListRow(title: "Airing at", content: Date(timeIntervalSince1970: TimeInterval(nextEpisode.airingAt)).toFullString())
             } header: {
                 Text("Airing schedule")
                     .textCase(nil)

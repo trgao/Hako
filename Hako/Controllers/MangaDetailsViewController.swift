2870be2ebd5024ff15bbb7c82645a88166a99ec3
trgao <tianrun.gao@gmail.com>
2025-06-16 16:37:30 +0800

    Fix retry for details view

--- Hako/Controllers/MangaDetailsViewController.swift
+++ Hako/Controllers/MangaDetailsViewController.swift
@@ -45,27 +45,27 @@ class MangaDetailsViewController: ObservableObject {
         isLoading = false
         
         // Load characters
-        try? await retry {
-            if let characters = networker.mangaCharactersCache[id] {
-                self.characters = characters
-            } else {
-                do {
+        do {
+            try await retry {
+                if let characters = networker.mangaCharactersCache[id] {
+                    self.characters = characters
+                } else {
                     let characters = try await networker.getMangaCharacters(id: id)
                     self.characters = characters
                     networker.mangaCharactersCache[id] = characters
-                } catch {
-                    print("Some unknown error occurred loading manga characters")
                 }
             }
+        } catch {
+            print("Some unknown error occurred loading manga characters")
         }
         
         // Load authors
-        try? await retry {
-            if let authors = networker.mangaAuthorsCache[id] {
-                self.authors = authors
-            } else {
-                var authors = manga?.authors ?? [];
-                do {
+        do {
+            try await retry {
+                if let authors = networker.mangaAuthorsCache[id] {
+                    self.authors = authors
+                } else {
+                    var authors = manga?.authors ?? [];
                     authors = try await authors.concurrentMap { author in
                         var newAuthor = author
                         let person = try await self.networker.getPersonDetails(id: author.id)
@@ -74,18 +74,18 @@ class MangaDetailsViewController: ObservableObject {
                     }
                     self.authors = authors
                     networker.mangaAuthorsCache[id] = authors
-                } catch {
-                    print("Some unknown error occurred loading manga authors")
                 }
             }
+        } catch {
+            print("Some unknown error occurred loading manga authors")
         }
         
         // Load related
-        try? await retry {
-            if let relatedItems = networker.mangaRelatedCache[id] {
-                self.relatedItems = relatedItems
-            } else {
-                do {
+        do {
+            try await retry {
+                if let relatedItems = networker.mangaRelatedCache[id] {
+                    self.relatedItems = relatedItems
+                } else {
                     let relations = try await networker.getMangaRelations(id: id)
                     var relatedItems = relations.filter{ $0.relation == "Prequel" || $0.relation == "Sequel" || $0.relation == "Adaptation" }.flatMap{ category in category.entry.map{ RelatedItem(malId: $0.malId, type: $0.type, title: $0.name, enTitle: nil, url: $0.url, relation: category.relation, imageUrl: nil) } }
                     relatedItems = try await relatedItems.concurrentMap { item in
@@ -103,28 +103,28 @@ class MangaDetailsViewController: ObservableObject {
                     }
                     self.relatedItems = relatedItems
                     networker.mangaRelatedCache[id] = relatedItems
-                } catch {
-                    print("Some unknown error occurred loading manga related")
                 }
             }
+        } catch {
+            print("Some unknown error occurred loading manga related items")
         }
         
         // Load reviews
-        try? await retry {
-            do {
+        do {
+            try await retry {
                 self.reviews = try await networker.getMangaReviewsList(id: id, page: 1)
-            } catch {
-                print("Some unknown error occurred loading manga reviews")
             }
+        } catch {
+            print("Some unknown error occurred loading manga reviews")
         }
         
         // Load statistics
-        try? await retry {
-            do {
+        do {
+            try await retry {
                 self.statistics = try await networker.getMangaStatistics(id: id)
-            } catch {
-                print("Some unknown error occurred loading manga statistics")
             }
+        } catch {
+            print("Some unknown error occurred loading manga statistics")
         }
     }
 }

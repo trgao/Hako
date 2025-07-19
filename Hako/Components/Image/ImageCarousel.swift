868cf28056a572c6f3f8779fd071391f61180c0d
trgao <tianrun.gao@gmail.com>
2025-07-19 13:39:24 +0800

    Fix save photos

--- Hako/Components/Image/ImageCarousel.swift
+++ Hako/Components/Image/ImageCarousel.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ImageCarousel: View {
     @Environment(\.displayScale) var displayScale
     @Environment(\.dismiss) private var dismiss
+    @State private var images: [UIImage?]
     @State private var isSuccessPresented = false
     @State private var isErrorPresented = false
     let pictures: [Picture]
     
     init(pictures: [Picture]?) {
         self.pictures = pictures ?? []
+        self.images = pictures?.map { _ in nil } ?? []
     }
     
     var body: some View {
@@ -27,16 +29,15 @@ struct ImageCarousel: View {
             }
             .padding([.horizontal, .top], 20)
             TabView {
-                ForEach(pictures.filter { $0.medium != nil }, id: \.medium) { item in
+                ForEach(Array(pictures.filter { $0.medium != nil }.enumerated()), id: \.0) { index, item in
                     let url = item.large == nil ? item.medium : item.large
-                    var inputImage: UIImage?
                     if let url = url {
                         AsyncImage(url: URL(string: url)!) { image in
                             image
                                 .resizable()
                                 .scaledToFill()
                                 .onAppear {
-                                    inputImage = image.render(scale: displayScale)
+                                    images[index] = image.render(scale: displayScale)
                                 }
                         } placeholder: {
                             Color.gray
@@ -44,13 +45,13 @@ struct ImageCarousel: View {
                         .frame(width: UIScreen.main.bounds.width * 9 / 10, height: (UIScreen.main.bounds.width * 9 / 10) / 150 * 212)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                         .contextMenu {
-                            Button {
-                                guard let inputImage = inputImage else { return }
-
-                                let imageSaver = ImageSaver(isSuccessPresented: $isSuccessPresented, isErrorPresented: $isErrorPresented)
-                                imageSaver.writeToPhotoAlbum(image: inputImage)
-                            } label: {
-                                Label("Save image", systemImage: "square.and.arrow.down")
+                            if let inputImage = images[index] {
+                                Button {
+                                    let imageSaver = ImageSaver(isSuccessPresented: $isSuccessPresented, isErrorPresented: $isErrorPresented)
+                                    imageSaver.writeToPhotoAlbum(image: inputImage)
+                                } label: {
+                                    Label("Save image", systemImage: "square.and.arrow.down")
+                                }
                             }
                         }
                     }

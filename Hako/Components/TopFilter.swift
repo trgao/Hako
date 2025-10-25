13a0565ea41ffd5a5fbc70960a777d9c8f22266b
trgao <tianrun.gao@gmail.com>
2025-10-25 21:34:21 +0800

    Add picker labels for macos

--- Hako/Components/TopFilter.swift
+++ Hako/Components/TopFilter.swift
@@ -17,26 +17,26 @@ struct TopFilter: View {
     var body: some View {
         Menu {
             if controller.type == .anime {
-                Picker(selection: $controller.animeRankingType, label: EmptyView()) {
+                Picker(selection: $controller.animeRankingType, label: Text("Rank type")) {
                     Label("All", systemImage: "circle.circle").tag("all")
                     Label("By popularity", systemImage: "popcorn").tag("bypopularity")
                     Label("By favourites", systemImage: "star").tag("favorite")
                 }
                 Divider()
-                Picker(selection: $controller.animeRankingType, label: EmptyView()) {
+                Picker(selection: $controller.animeRankingType, label: Text("Media type")) {
                     Label("TV", systemImage: "tv").tag("tv")
                     Label("OVA", systemImage: "tv").tag("ova")
                     Label("Movie", systemImage: "movieclapper").tag("movie")
                     Label("Special", systemImage: "sparkles.tv").tag("special")
                 }
             } else if controller.type == .manga {
-                Picker(selection: $controller.mangaRankingType, label: EmptyView()) {
+                Picker(selection: $controller.mangaRankingType, label: Text("Rank type")) {
                     Label("All", systemImage: "circle.circle").tag("all")
                     Label("By popularity", systemImage: "popcorn").tag("bypopularity")
                     Label("By favourites", systemImage: "star").tag("favorite")
                 }
                 Divider()
-                Picker(selection: $controller.mangaRankingType, label: EmptyView()) {
+                Picker(selection: $controller.mangaRankingType, label: Text("Media type")) {
                     Label("Manga", systemImage: "book").tag("manga")
                     Label("Novels", systemImage: "book.closed").tag("novels")
                     Label("One Shots", systemImage: "book.pages").tag("oneshots")

7c843411a6e26c458c1ad0d3f1372204a174db44
trgao <tianrun.gao@gmail.com>
2025-10-26 14:04:31 +0800

    Add animation to user favourites

--- Hako/Controllers/ProfileViewController.swift
+++ Hako/Controllers/ProfileViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Gao Tianrun on 22/11/24.
 //
 
-import Foundation
+import SwiftUI
 
 @MainActor
 class ProfileViewController: ObservableObject {
@@ -23,15 +23,20 @@ class ProfileViewController: ObservableObject {
         }
         
         do {
-            self.userFavourites = try await networker.getUserFavourites()
-            self.anime = try await userFavourites?.anime.concurrentMap { anime in
+            let userFavourites = try await networker.getUserFavourites()
+            let anime = try await userFavourites.anime.concurrentMap { anime in
                 let anime = try await NetworkManager.shared.getAnimeDetails(id: anime.id)
                 return MALListAnime(anime: anime)
-            } ?? []
-            self.manga = try await userFavourites?.manga.concurrentMap { manga in
+            }
+            let manga = try await userFavourites.manga.concurrentMap { manga in
                 let manga = try await NetworkManager.shared.getMangaDetails(id: manga.id)
                 return MALListManga(manga: manga)
-            } ?? []
+            }
+            withAnimation {
+                self.userFavourites = userFavourites
+                self.anime = anime
+                self.manga = manga
+            }
         } catch {
             print("Some unknown error occurred loading user favourites")
         }

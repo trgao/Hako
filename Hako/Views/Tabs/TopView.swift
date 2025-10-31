940612d4bf435f517f4afb8dd965cb59171c82f1
trgao <tianrun.gao@gmail.com>
2025-10-31 22:34:02 +0800

    Refactor filter and change favourite icon

--- Hako/Views/Tabs/TopView.swift
+++ Hako/Views/Tabs/TopView.swift
@@ -29,6 +29,47 @@ struct TopView: View {
         }
     }
     
+    var filter: some View {
+        Menu {
+            if controller.type == .anime {
+                Picker(selection: $controller.animeRankingType, label: Text("Rank type")) {
+                    Label("All", systemImage: "star").tag("all")
+                    Label("TV", systemImage: "tv").tag("tv")
+                    Label("OVA", systemImage: "tv").tag("ova")
+                    Label("Movie", systemImage: "movieclapper").tag("movie")
+                    Label("Special", systemImage: "sparkles.tv").tag("special")
+                    Label("Popularity", systemImage: "popcorn").tag("bypopularity")
+                    Label("Favourites", systemImage: "heart").tag("favorite")
+                }
+            } else if controller.type == .manga {
+                Picker(selection: $controller.mangaRankingType, label: Text("Rank type")) {
+                    Label("All", systemImage: "star").tag("all")
+                    Label("Manga", systemImage: "book").tag("manga")
+                    Label("Novels", systemImage: "book.closed").tag("novels")
+                    Label("One Shots", systemImage: "book.pages").tag("oneshots")
+                    Label("Manhwa", systemImage: "book").tag("manhwa")
+                    Label("Manhua", systemImage: "book").tag("manhua")
+                    Label("Popularity", systemImage: "popcorn").tag("bypopularity")
+                    Label("Favourites", systemImage: "heart").tag("favorite")
+                }
+            }
+        } label: {
+            Button{} label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+            }
+        }
+        .onChange(of: controller.animeRankingType) {
+            Task {
+                await controller.refresh()
+            }
+        }
+        .onChange(of: controller.mangaRankingType) {
+            Task {
+                await controller.refresh()
+            }
+        }
+    }
+    
     var body: some View {
         NavigationStack {
             VStack {
@@ -92,7 +133,7 @@ struct TopView: View {
             }
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
-                    TopFilter(controller: controller)
+                    filter
                         .disabled(controller.isLoading)
                 }
                 ToolbarItem(placement: .topBarTrailing) {

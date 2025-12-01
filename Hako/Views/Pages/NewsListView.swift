98b2498f916e76e1cc890d6723603030ca8083b0
trgao <tianrun.gao@gmail.com>
2025-12-01 18:34:53 +0800

    Add share context menu to news

--- Hako/Views/Pages/NewsListView.swift
+++ Hako/Views/Pages/NewsListView.swift
@@ -18,24 +18,30 @@ struct NewsListView: View {
             } else {
                 List {
                     ForEach(controller.news.filter{ URL(string: $0.link ?? "") != nil }, id: \.hashValue) { item in
-                        Link(destination: URL(string: item.link!)!) {
-                            HStack {
-                                ImageFrame(id: "news\(item.hashValue)", imageUrl: item.media?.thumbnails?.first?.text, imageSize: .small)
-                                VStack(alignment: .leading) {
-                                    Text(item.title ?? "")
-                                        .lineLimit(settings.getLineLimit())
-                                        .bold()
-                                        .font(.system(size: 16))
-                                        .foregroundStyle(Color.primary)
-                                    Text(item.pubDate?.toString() ?? "")
-                                        .opacity(0.7)
-                                        .font(.system(size: 13))
-                                        .padding(.top, 1)
-                                        .foregroundStyle(Color.primary)
+                        if let link = item.link {
+                            Link(destination: URL(string: link)!) {
+                                HStack {
+                                    ImageFrame(id: "news\(item.hashValue)", imageUrl: item.media?.thumbnails?.first?.text, imageSize: .small)
+                                    VStack(alignment: .leading) {
+                                        Text(item.title ?? "")
+                                            .lineLimit(settings.getLineLimit())
+                                            .bold()
+                                            .font(.system(size: 16))
+                                            .foregroundStyle(Color.primary)
+                                        Text(item.pubDate?.toString() ?? "")
+                                            .opacity(0.7)
+                                            .font(.system(size: 13))
+                                            .padding(.top, 1)
+                                            .foregroundStyle(Color.primary)
+                                    }
+                                    .padding(5)
                                 }
-                                .padding(5)
+                                .contextMenu {
+                                    ShareLink(item: URL(string: link)!) {
+                                        Label("Share", systemImage: "square.and.arrow.up")
+                                    }
+                                }
                             }
-                            .contentShape(Rectangle())
                         }
                     }
                 }
